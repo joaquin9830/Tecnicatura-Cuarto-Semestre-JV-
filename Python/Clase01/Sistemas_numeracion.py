6cf60ac7f4bfe1d4dc083dd3f45270fae32b0e22
joaquin9830 <joaquin9811@gmail.com>
2024-08-28 17:04:42 -0300

    Clase 2 Python | sistema numerico

---
+++ Python/Clase01/Sistemas_numeracion.py
@@ -0,0 +1,32 @@
+#Profundizando en los sistemas de numeración
+
+#Sistema decimal
+
+a = 10
+print(f"a: {a}")
+
+#Sistema binario
+
+a = 0b1010
+print(f"a: {a}")
+
+#Sistema octal
+
+a = 0o12
+print(f"a: {a}")
+
+#Hexagecimal
+a = 0xA
+print(f"a hexadecimal: {a}")
+
+#Base Decimal
+a = int('23', 10)
+print(f"a decimal: {a}")
+
+#Base Binario
+a = int('1010', 2)
+print(f"a binario: {a}")
+
+#Base Octal
+a = int('12', 8)
+print(f"a octal: {a}")
