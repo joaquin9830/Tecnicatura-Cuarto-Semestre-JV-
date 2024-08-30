04f3738ab14ea452a9fec0bde2be8c09a3b22dd7
joaquin9830 <joaquin9811@gmail.com>
2024-08-30 19:37:25 -0300

    Creación de valores_infinitos | Clase 02 Python

---
+++ Python/Clase01/valores_infinitos.py
@@ -0,0 +1,6 @@
+#Manejo de valores infinitos
+import math
+
+infinito_positivo = float('inf')
+print(f'Infinito positivo: {infinito_positivo}')
+print(f'Es infinito: {math.isinf(infinito_positivo)}')
