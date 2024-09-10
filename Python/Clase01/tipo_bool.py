3dd518ce4d69e46822152319e7bb1d251342f27f
joaquin9830 <joaquin9811@gmail.com>
2024-09-10 19:52:57 -0300

    Clase 03 Python | Tipo Bool

---
+++ Python/Clase01/tipo_bool.py
@@ -0,0 +1,10 @@
+
+#Bool con tiene los valores de True y False
+#Los tipos numéticos, es flase para el 0 (cero), true para los demás valores
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 15
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
