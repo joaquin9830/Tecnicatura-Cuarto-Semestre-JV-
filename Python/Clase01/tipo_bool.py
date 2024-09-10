eed35b7dbd951998bb6bf8aac26639983a0325a0
joaquin9830 <joaquin9811@gmail.com>
2024-09-10 20:01:39 -0300

    Clase 03 Python | Lista | Tupla | Diccionario | Sentencias de control

--- Python/Clase01/tipo_bool.py
+++ Python/Clase01/tipo_bool.py
@@ -13,3 +13,42 @@ print(f'valor: {valor}, Resultado: {resultado}')
 valor = ''
 resultado = bool(valor)
 print(f'valor: {valor}, Resultado: {resultado}')
+
+#Tipo de colecciones -> False para colecciones vacias
+#Tipo de colecciones -> True para todas las demás
+#Lista
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacía: {valor}, resultado: {resultado}')
+
+
+valor = [2,3,4]
+resultado = bool(valor)
+print(f'valor de una lista llena: {valor}, resultado: {resultado}')
+
+#Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacía: {valor}, resultado: {resultado}')
+
+
+valor = (2,)
+resultado = bool(valor)
+print(f'valor de una tupla llena: {valor}, resultado: {resultado}')
+
+
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacía: {valor}, resultado: {resultado}')
+
+valor = {'Nombre: Zeus'}
+resultado = bool(valor)
+print(f'valor de un diccionario lleno: {valor}, resultado: {resultado}')
+
+#Sentencias de control con bool
+if '':
+    print('Regresa verdadero')
+
+else:
+    print('Regresa falso')
