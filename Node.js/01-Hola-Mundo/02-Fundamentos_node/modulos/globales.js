bb41931cbaa2ae9309baf7bed41c240e60355961
joaquin9830 <joaquin9811@gmail.com>
2024-11-10 10:57:34 -0300

    Node Clase 13

---
+++ Node.js/01-Hola-Mundo/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,36 @@
+// this === global = true
+
+//Mostrar algo en consola
+//console.log();
+
+//Mostrar un mensaje en forma de error
+//console.error();
+
+//Ejecutamos un codigo despues de un intervalo de tiempo
+//setTimeout(() => { });
+
+//Ejecutamos un codigo cada intervalo de tiempo
+//setInterval(() => { });
+
+//Da prioridad de ejecucion a una funcion asincronica
+//setImmdiate(() => { });
+
+//console.log(setInterval);
+
+let i = 0;
+let intervalo = setInterval(() => {
+    console.log('Hola');
+    if (i === 3) {
+        clearInterval(intervalo); //detenemos al interval
+    }
+    i++;
+}, 1000);
+
+setImmediate(() => {
+    console.log('Saludo inmediato');
+});
+
+console.log(__filename);
+
+global.miVariable = 'mi variable global';
+console.log(miVariable);
